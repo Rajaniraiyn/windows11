270a0b793eb4f22ae42a1e04c7a4816eefb66e5a
ShudarsanRegmi <shudarsanregmi555@gmail.com>
2021-07-07 19:51:22 +0545

    Desktop icons tweaks | made search Icon functional | styled more options button in start menu

--- src/start.js
+++ src/start.js
@@ -1,15 +1,23 @@
 document.addEventListener("click", (e) => {
     if (e.target == document.querySelector(".center img:nth-child(1)")) {
         document.querySelector(".start").classList.add("show-start");
+    } else if (e.target == document.getElementById("searchIcon")) {
+        // making the search button at the taskbar work
+        console.log("search icon clicked");
+        document.querySelector(".start").classList.add("show-start");
+        document.getElementById("searchBar").focus();
     }
     // ? close the menu if the user clicks outside of it
-    else if (e.target.offsetParent != document.querySelector(".start")) {
+    else if (e.target == document.getElementById("searchBar")) {
+        console.log("searching for you");
+        // handling the weird logic of closing start menu while clicking on searchIcon
+    } else if (e.target.offsetParent != document.querySelector(".start")) {
         document.querySelector(".start").classList.remove("show-start");
     };
 });
 let dt = new Date().toLocaleString().split(", ");
-document.querySelector(".time-date > span:nth-child(2)").innerHTML = dt[0];
-document.querySelector(".time-date > span:nth-child(1)").innerHTML = dt[1];
+document.querySelector(".time-date > span:nth-child(2)").innerText = dt[0];
+document.querySelector(".time-date > span:nth-child(1)").innerText = dt[1];
 setInterval(() => {
-    document.querySelector(".time-date > span:nth-child(1)").innerHTML = new Date().toLocaleString().split(", ")[1];
+    document.querySelector(".time-date > span:nth-child(1)").innerText = new Date().toLocaleString().split(", ")[1];
 }, 1e3)
