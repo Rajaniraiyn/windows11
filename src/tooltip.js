d5fe968c3bd5169220cd10529c8c8ed3c1fa8b2b
Kush Agarwal <83411262+Kushhhh-sh@users.noreply.github.com>
2021-07-14 11:50:56 +0530

    Added Tooltips

---
+++ src/tooltip.js
@@ -0,0 +1,68 @@
+tippy('#deskbrowser', {
+    content: 'Microsoft Edge',
+    placement: 'right',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#deskvscode', {
+    content: 'VS Code',
+    placement: 'right',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#deskcomp', {
+    content: 'This PC',
+    placement: 'right',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#starticon', {
+    content: 'Start',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#searchIcon', {
+    content: 'Search',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#taskviewIcon', {
+    content: 'Task View',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#widgetsIcon', {
+    content: 'Widgets',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#teamsIcon', {
+    content: 'Microsoft Teams',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#filesIcon', {
+    content: 'File Explorer',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#edgeIcon', {
+    content: 'Microsoft Edge',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#storeIcon', {
+    content: 'Microsoft Store',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('#keyboardOpener', {
+    content: 'Shortcuts Keyboard',
+    theme: 'light',
+    delay: [1000, 100]
+});
+tippy('.action-center-button', {
+    content: 'Action Center',
+    theme: 'light',
+    delay: [1000, 100]
+});
